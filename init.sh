a37e03b4732b309cbcfd8d188261f15e73ddef30
iz01 <47084737+iz01@users.noreply.github.com>
2020-04-05 18:56:36 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 timedatectl set-timezone Europe/Budapest
 yum update -y
 yum install -y mc htop wget mlocate nmap-ncat
-echo 'color murphy' >> ~/.vimrc
+echo -e 'color murphy\nset tabstop=2 shiftwidth=2 expandtab\nset hlsearch' >> ~/.vimrc
 echo 'export HISTTIMEFORMAT="%y.%m.%d %T "' >> .bash_profile
