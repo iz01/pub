09c0f06ddd4ae3b2caa6fdaa544415f0cb2c1071
iz01 <47084737+iz01@users.noreply.github.com>
2020-04-05 19:00:29 +0200

    Update init-ubi.sh

--- init-ubi.sh
+++ init-ubi.sh
@@ -1,5 +1,5 @@
 timedatectl set-timezone Europe/Budapest
 apt update && apt upgrade -y
-apt install -y mc htop wget mlocate nmap-ncat
+apt install -y mc htop wget mlocate netcat
 echo 'color murphy' >> ~/.vimrc
 echo 'export HISTTIMEFORMAT="%y.%m.%d %T "' >> .bash_profile
