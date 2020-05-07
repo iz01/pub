08b6544233a4fe1d4785f81792ffec4dbaa2d415
iz01 <47084737+iz01@users.noreply.github.com>
2020-05-07 20:17:41 +0200

    Update init-ubi.sh

--- init-ubi.sh
+++ init-ubi.sh
@@ -7,9 +7,12 @@ apt update && apt upgrade -y
 apt install -y mc htop wget mlocate netcat vim
 
 #shell settings
-echo -e 'color murphy\nset tabstop=2 shiftwidth=2 expandtab\nset hlsearch' >> ~/.vimrc
 echo 'export HISTTIMEFORMAT="%F %T "' >> .bash_profile
 echo 'export PS1="\t \[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "' >> .bash_profile
+echo "alias ll='ls -lah'" >> .bashrc
+
+#vim settings
+echo -e 'color murphy\nset tabstop=2 shiftwidth=2 expandtab\nset hlsearch' >> ~/.vimrc
 
 #indexing for locate db
 updatedb
