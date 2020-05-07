bc56bb7ef39ab451b83e3670dfc1613a30cc3454
iz01 <47084737+iz01@users.noreply.github.com>
2020-05-07 20:09:12 +0200

    Update init-ubi.sh

--- init-ubi.sh
+++ init-ubi.sh
@@ -1,5 +1,15 @@
+#regional settings
 timedatectl set-timezone Europe/Budapest
+localectl set-keymap hu
+
+#os upgrade+tools install
 apt update && apt upgrade -y
-apt install -y mc htop wget mlocate netcat
+apt install -y mc htop wget mlocate netcat vim
+
+#shell settings
 echo -e 'color murphy\nset tabstop=2 shiftwidth=2 expandtab\nset hlsearch' >> ~/.vimrc
-echo 'export HISTTIMEFORMAT="%y.%m.%d %T "' >> .bash_profile
+echo 'export HISTTIMEFORMAT="%F %T "' >> .bash_profile
+echo 'export PS1="\t \[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "' >> .bash_profile
+
+#indexing for locate db
+updatedb
